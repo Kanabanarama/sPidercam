867ac1c2cc1b4ae66c9809aa4eb6c7413270a19b
Kana <kanabanarama@googlemail.com>
2018-04-16 00:51:23 +0200

    pylinted serve.py

--- serve.py
+++ serve.py
@@ -1,43 +1,55 @@
-from flask import Flask, render_template, send_from_directory, jsonify, Response
+"""
+Serve html files and media for web frontend and json for mobile app
+"""
+
 import os
-import io
-import time
 import datetime
-import picamera
+from flask import Flask, render_template, send_from_directory, jsonify
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-livestream = ['stream.mjpg']
+def get_livestream_filename():
+    "Returns the stream filename"
+    return ['stream.mjpg']
 
-def getVideos():
+def get_video_dirs():
+    "Returns the folders containing timelapse videos (and their thumbnails)"
     #videos = [folder + '/video.mp4' for folder in os.listdir('timelapse/videos')]
     #print(videos[:])
     return os.listdir('timelapse/videos')
 
-@app.template_filter('to_day_name')
-def to_day_name_filter(s):
-    weekday = datetime.datetime.strptime(s, '%Y-%m-%d').strftime('%a')
+@APP.template_filter('to_day_name')
+def to_day_name_filter(datestring):
+    "Return the weekday from a datestring in Y-m-d form"
+    weekday = datetime.datetime.strptime(datestring, '%Y-%m-%d').strftime('%a')
     return weekday
 
-@app.route('/timelapse/videos/<path:path>')
+@APP.route('/timelapse/videos/<path:path>')
 def send_thumbnail(path):
+    "Serve video and thumbnail files"
     return send_from_directory('timelapse/videos', path)
 
-@app.route('/')
+@APP.route('/')
 def index():
-    return render_template('index.html', livestream = livestream, videos = getVideos())
+    "Render index template"
+    return render_template('index.html',
+                           livestream_filename=get_livestream_filename(),
+                           video_dirs=get_video_dirs())
 
-@app.route('/json')
+@APP.route('/json')
 def json_index():
+    "Return json index"
     return jsonify('hello spider!')
 
-@app.route('/json/livestream')
+@APP.route('/json/livestream')
 def json_livestream():
-    return jsonify(livestream)
+    "Return livestream filename as json"
+    return jsonify(get_livestream_filename())
 
-@app.route('/json/timelapse')
+@APP.route('/json/timelapse')
 def json_timelapse():
-    return jsonify(getVideos())
+    "Return timelapse video folders as json"
+    return jsonify(get_video_dirs())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    APP.run(host='0.0.0.0', port=5000, debug=True)
