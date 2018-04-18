49808a862e315df5b0de6b49223256892233e18d
Kana <kanabanarama@googlemail.com>
2018-04-19 00:33:55 +0200

    threads for timelapse capturing and merging

--- serve.py
+++ serve.py
@@ -3,40 +3,51 @@ Serve html files and media for web frontend and json for mobile app
 """
 
 import os
-import datetime
-from flask import Flask, render_template, send_from_directory, jsonify, Response
-
+import threading
 import io
 import time
+import datetime
 import picamera
+import timelapse
+from flask import Flask, render_template, send_from_directory, jsonify, Response
 
 APP = Flask(__name__)
+CAMERA = {}
+
+def init_camera():
+    #if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+    if(not len(CAMERA)):
+        camera_pi = picamera.PiCamera()
+        camera_pi.resolution = (1024, 576)
+        camera_pi.framerate = 30
+        CAMERA['picamera'] = camera_pi
+    return CAMERA
+
+def frame_generator():
+    """Camera stream generator function"""
+    camera = init_camera()['picamera']
+    stream = io.BytesIO()
+    for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True, splitter_port=2):
+        stream.seek(0)
+        frame = stream.read()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        stream.seek(0)
+        stream.truncate()
 
 def get_livestream_filename():
     "Returns the stream filename"
     return ['stream.mjpg']
-
-def frameGenerator():
-    """Camera stream generator function"""
-    with picamera.PiCamera() as camera:
-        camera.resolution = (640, 360)
-        camera.framerate = 30
-        time.sleep(1)
-        stream = io.BytesIO()
-        for ioBytes in camera.capture_continuous(stream, 'jpeg',
-                                                 use_video_port=True):
-            stream.seek(0)
-            frame = stream.read()
-            yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            stream.seek(0)
-            stream.truncate()
 
 def get_video_dirs():
     "Returns the folders containing timelapse videos (and their thumbnails)"
     #videos = [folder + '/video.mp4' for folder in os.listdir('timelapse/videos')]
     #print(videos[:])
     return os.listdir('timelapse/videos')
+
+def get_frame_dirs():
+    "Returns the folders containing timelapse frames"
+    return os.listdir('timelapse/frames')
 
 @APP.template_filter('to_day_name')
 def to_day_name_filter(datestring):
@@ -58,7 +69,8 @@ def index():
 
 @APP.route('/livestream')
 def livestream():
-    return Response(frameGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    "Send the camera livestream"
+    return Response(frame_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @APP.route('/json')
 def json_index():
@@ -75,5 +87,33 @@ def json_timelapse():
     "Return timelapse video folders as json"
     return jsonify(get_video_dirs())
 
+def merge_worker():
+    """Interval check frames ready to video merging"""
+    if(os.path.isdir('timelapse/frames')):
+        while True:
+            print('--- Watching queue for finished frame captures to merge ---')
+            mergedVideo = timelapse.Timelapse().merge()
+            if(mergedVideo):
+                timelapse.Timelapse().create_thumbnail(mergedVideo)
+            time.sleep(3600)
+
+def capture_timelapse():
+    """Capture frames for the rest of the day"""
+    intervalBetweenShots = 1
+    now = datetime.datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    remainingSeconds = (midnight - now).seconds
+    remainingShots = round(remainingSeconds / intervalBetweenShots)
+    dirName = now.strftime("%Y-%m-%d")
+    camera = init_camera()['picamera']
+    while True:
+        timelapse.Timelapse().capture(camera, remainingShots, intervalBetweenShots, dirName)
+
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5000, debug=True)
+    p1 = threading.Thread(target=capture_timelapse)
+    p1.start()
+    #p1.join()
+    p2 = threading.Thread(target=merge_worker)
+    p2.start()
+    #p2.join()
+    APP.run(host='0.0.0.0', port=5000, debug=False)
--- timelapse.py
+++ timelapse.py
@@ -1,39 +1,75 @@
 import os
+import re
 import ffmpy
-from time import sleep
+import collections
+from time import sleep, time
 
 class Timelapse:
-    def capture(self, device, shots, interval, outputPath):
-        if(os.path.isdir(outputPath)):
-            for i in os.listdir(outputPath):
-                os.remove(os.path.join(outputPath, i))
-            os.rmdir(outputPath)
-            print("removed existing directory [%s]" % outputPath)
+    frame_path = 'timelapse/frames'
+    video_path = 'timelapse/videos'
+    merge_queue = collections.OrderedDict()
 
-        os.makedirs(outputPath)
+    def set_frame_path(self, path):
+        self.frame_path = path
 
-        print("capturing %i images in intervals of %i seconds"
+    def set_video_path(self, path):
+        self.video_path = path
+
+    def get_highest_frame_number(self, frame_dir):
+        list_of_files = os.listdir(frame_dir)
+        max_framenum = 1
+        for filename in list_of_files:
+            current_framenum = int(re.search('\d+', filename).group(0))
+            max_framenum = max(current_framenum, max_framenum)
+        return max_framenum + 1
+
+    def capture(self, device, shots, interval, dirName):
+        outputPath = self.frame_path + '/' + dirName
+        os.makedirs(outputPath, exist_ok=True)
+
+        print("--- Capturing %i images in intervals of %i seconds ---"
             % (shots, interval)
         )
 
+        latestFrameNum = self.get_highest_frame_number(outputPath)
+        print("--- Beginning from file with number %i ---" % latestFrameNum)
+
         for i in range(shots):
-            frameNum = i + 1
+            frameNum = latestFrameNum + i
             frameFile = "frame%i.jpg" % frameNum
             # using the still port would cause dropped frames due to camera mode change
-            device.capture('%s/%s' % (outputPath, frameFile), use_video_port=True)
-            print("> captured %s (remaining: %i)" % (frameFile, shots - frameNum))
+            device.capture('%s/%s' % (outputPath, frameFile), use_video_port=True, splitter_port=1)
+            print("> captured %s (remaining: %i)" % (frameFile, shots - i + 1))
             sleep(interval)
 
-    def merge(self, framePath, outputPath):
-        timelapseFile = "video.mp4"
+        print('--- Finished frame capturing for [%s], put into queue for merging' % outputPath)
+        self.merge_queue[outputPath] = self.video_path + '/' + dirName
 
-        os.makedirs(outputPath, exist_ok=True)
+    #@staticmethod
+    def merge(self, inputFramePath=None, outputVideoPath=None): #inputFramePath, outputVideoPath):
+        if(len(self.merge_queue)):
+            nextElement = self.merge_queue.popitem()
+            print('--- Merge next queue item: %s ---' % nextElement)
+            framePath = nextElement[0]
+            outputPath = nextElement[1]
 
+            os.makedirs(outputPath, exist_ok=True)
+            ff = ffmpy.FFmpeg(
+                 inputs={'%s/frame%%01d.jpg' % framePath: '-f image2 -r 30/1'},
+                 outputs={'%s/video.mp4' % outputPath: '-y -vcodec libx264 -pix_fmt yuv420p -crf 18 -preset medium'} #veryslow
+            )
+            ff.run()
+            # as command:
+            #os.system("ffmpeg -f image2 -r 30/1 -i %s/frame%%01d.png -vcodec mpeg4 -y %s/video.mp4" % (framePath, timelapsePath))
+            return '%s/video.mp4' % outputPath
+
+    #@staticmethod
+    def create_thumbnail(videoPath):
         ff = ffmpy.FFmpeg(
-             inputs={'%s/frame%%01d.jpg' % framePath: '-f image2 -r 30/1'},
-             outputs={'%s/video.mp4' % outputPath: '-y -vcodec libx264 -crf 18 -preset veryslow'}
+             inputs={'%s/video.mp4' % videoPath: None},
+             outputs={'%s/thumbnail.jpg' % outputPath: '-ss 0:0:00 -vframes 1 -q:v 2'}
         )
         ff.run()
-
         # as command:
-        #os.system("ffmpeg -f image2 -r 30/1 -i %s/frame%%01d.png -vcodec mpeg4 -y %s/video.mp4" % (framePath, timelapsePath))
+        #os.system("ffmpeg -i video.mp4 -ss 00:00:00 -vframes 1 -q:v 2 screenshot.jpg")
+        return '%s/thumbnail.jpg' % outputPath
